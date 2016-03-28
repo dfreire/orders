ee0ee69a63a6fbcd19fc673c6e6c7c2fa736aa9a
dfreire <dario.freire@gmail.com>
2016-03-28 22:36:11 +0100

    updat

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
+interface Customer {
+    id: string;
+    basket: Basket;
+}
+
 interface Address {
+    id: string;
+    customerId: string,
     fullName: string;
     streetLine1: string;
     streetLine2?: string;
@@ -20,67 +27,17 @@ interface ShippingAddress extends Address {
     isDefaultShippingAddress: boolean;
 }
 
-export class Customer {
-    id: string;
-    shoppingCart: ShoppingCart;
-    addresses: Address[];
-
-    constructor(id: string) {
-        this.id = id;
-        this.shoppingCart = new ShoppingCart(id);
-        this.addresses = new Array<Address>();
-    };
-
-    addAddress(address: Address) {
-        this.addresses.push(address);
-        // TODO ajax request
-    }
-
-    removeAddress(address: Address) {
-        // TODO ajax request
-    }
-
-    getDefaultBillingAddress(): BillingAddress {
-        return <BillingAddress>_.find(this.addresses, (address) => {
-            return /*(address instanceof BillingAddress) &&*/ (<BillingAddress>address).isDefaultBillingAddress;
-        });
-    }
-
-    getDefaultShippingAddress(): ShippingAddress {
-        return <ShippingAddress>_.find(this.addresses, (address) => {
-            return /*(address instanceof ShippingAddress) &&*/ (<ShippingAddress>address).isDefaultShippingAddress;
-        });
-    }
-}
-
-export class ShoppingCart {
+interface Basket {
     customerId: string;
-    items: ShoppingCartItem[];
-
-    constructor(customerId: string) {
-        this.customerId = customerId;
-        this.items = [];
-    }
-
-    addItem(id: string, quantity: number) {
-        this.items.push({ id: id, quantity: quantity });
-    }
-
-    checkout(): Order {
-        const order = new Order(this.customerId);
-        _.each(this.items, (item) => {
-            order.addItem(item.id, item.quantity);
-        });
-        return order;
-    }
+    items: BasketItem[];
 }
 
-interface ShoppingCartItem {
+interface BasketItem {
     id: string;
     quantity: number;
 }
 
-export class Order {
+interface Order {
     date: Date;
     status: OrderStatus;
     customerId: string;
@@ -89,17 +46,6 @@ export class Order {
     // copyOfShippingRate: ShippingRate;
     items: OrderItem[];
     total: number;
-
-    constructor(customerId: string) {
-        this.date = new Date();
-        this.status = OrderStatus.PendingPayment;
-        this.customerId = customerId;
-        this.items = new Array<OrderItem>();
-    }
-
-    addItem(id: string, quantity: number) {
-        this.items.push({ id: id, quantity: quantity });
-    }
 }
 
 enum OrderStatus { PendingPayment, Payed, PendingShipping, Shipped }
@@ -124,4 +70,57 @@ interface SellableItem {
 interface SellableItemPrice {
     value: number;
     vatRegionIds: string[];
+}
+
+function addCustomerAddress(address: Address) {
+    // TODO
+}
+
+function removeCustomerAddress(addressId: string) {
+    // TODO
+}
+
+function getDefaultCustomerBillingAddress(): BillingAddress {
+    // TODO
+    return null;
+}
+
+function getDefaultCustomerShippingAddress(): ShippingAddress {
+    // TODO
+    return null;
+}
+
+function getCustomerBillingAddresses(): BillingAddress[] {
+    // TODO
+    return null
+}
+
+function getCustomerShippingAddresses(): ShippingAddress[] {
+    // TODO
+    return null
+}
+
+function putInBasket(itemId: string, quantity: number) {
+    // TODO
+}
+
+function removeFromBasket(itemId: string, quantity: number) {
+    // TODO
+}
+
+function checkout(): Order {
+    // TODO
+    return null;
+}
+
+function setOrderShippingAddress(shippingAddress: ShippingAddress) {
+    // TODO
+}
+
+function setOrderBillingAddress(billingAddress: BillingAddress) {
+    // TODO
+}
+
+function confirmCheckout() {
+    // TODO
 }
