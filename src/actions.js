0d0c538cb4f182a4d7c7734f09a961af6ebaef23
dfreire <dario.freire@gmail.com>
2016-03-31 22:38:18 +0100

    update

--- src/actions.js
+++ src/actions.js
@@ -8,11 +8,29 @@ export function putInCurrentOrder(sellableItemId: string, quantity: number) {
     };
 }
 
+const REMOVE_FROM_CURRENT_ORDER = "REMOVE_FROM_CURRENT_ORDER";
+
 export function removeFromCurrentOrder(sellableItemId: string) {
+    return {
+        type: REMOVE_FROM_CURRENT_ORDER,
+        sellableItemId: sellableItemId
+    };
 }
+
+const SET_CURRENT_ORDER_SHIPPING_ADDRESS = "SET_CURRENT_ORDER_SHIPPING_ADDRESS";
 
 export function setCurrentOrderShippingAddress(shippingAddressId: string) {
+    return {
+        type: SET_CURRENT_ORDER_SHIPPING_ADDRESS,
+        shippingAddressId: shippingAddressId
+    };
 }
 
+const SET_CURRENT_ORDER_BILLING_ADDRESS = "SET_CURRENT_ORDER_BILLING_ADDRESS";
+
 export function setCurrentOrderBillingAddress(billingAddressId: string) {
+    return {
+        type: SET_CURRENT_ORDER_BILLING_ADDRESS,
+        billingAddressId: billingAddressId
+    };
 }
