c2b149c38db6e609533e0b62aa1e04830aba8272
dfreire <dario.freire@gmail.com>
2016-04-01 22:31:23 +0100

    update

--- src/reducers.ts
+++ src/reducers.ts
@@ -4,7 +4,7 @@ import { Action } from "./actions";
 import {ADD_TO_SHOPPING_BASKET, AddToShoppingBasketAction, onAddToShoppingBasket} from "./add-to-shopping-basket";
 import {REMOVE_FROM_SHOPPING_BASKET, RemoveFromShoppingBasketAction, onRemoveFromShoppingBasket} from "./remove-from-shopping-basket";
 import {SET_ORDER_BILLING_ADDRESS, SetOrderBillingAddressAction, onSetOrderBillingAddress} from "./set-order-billing-address";
-import {SET_ORDER_SHIPPING_ADDRESS, SetOrderShippingAddressAction, setOrderShippingAddress} from "./set-order-shipping-address";
+import {SET_ORDER_SHIPPING_ADDRESS, SetOrderShippingAddressAction, onSetOrderShippingAddress} from "./set-order-shipping-address";
 
 const newState: State = {
     user: {
@@ -21,12 +21,12 @@ export function app(state: State = newState, action: Action): State {
     switch (action.type) {
         case ADD_TO_SHOPPING_BASKET:
             return onAddToShoppingBasket(state, action as AddToShoppingBasketAction);
-        case actions.REMOVE_FROM_CURRENT_ORDER:
-            break;
-        case actions.SET_CURRENT_ORDER_SHIPPING_ADDRESS:
-            break;
-        case actions.SET_CURRENT_ORDER_BILLING_ADDRESS:
-            break;
+        case REMOVE_FROM_SHOPPING_BASKET:
+            return onRemoveFromShoppingBasket(state, action as RemoveFromShoppingBasketAction);
+        case SET_ORDER_BILLING_ADDRESS:
+            return onSetOrderBillingAddress(state, action as SetOrderBillingAddressAction);
+        case SET_ORDER_SHIPPING_ADDRESS:
+            return onSetOrderShippingAddress(state, action as SetOrderShippingAddressAction);
         default:
             return state;
     }
