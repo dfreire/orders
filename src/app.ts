a0339508107b065fce92c8b7b6e23408c06a827e
dfreire <dario.freire@gmail.com>
2016-04-04 23:09:38 +0100

    update

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import { Action, State } from "./types";
 
-import {ADD_TO_SHOPPING_BASKET, AddToShoppingBasketAction, onAddToShoppingBasket} from "./add-to-shopping-basket";
-import {REMOVE_FROM_SHOPPING_BASKET, RemoveFromShoppingBasketAction, onRemoveFromShoppingBasket} from "./remove-from-shopping-basket";
-import {SET_ORDER_BILLING_ADDRESS, SetOrderBillingAddressAction, onSetOrderBillingAddress} from "./set-order-billing-address";
-import {SET_ORDER_SHIPPING_ADDRESS, SetOrderShippingAddressAction, onSetOrderShippingAddress} from "./set-order-shipping-address";
+import {ADD_TO_SHOPPING_BASKET, AddToShoppingBasketAction, onAddToShoppingBasket} from "./shopping-basket/add-to-shopping-basket";
+import {REMOVE_FROM_SHOPPING_BASKET, RemoveFromShoppingBasketAction, onRemoveFromShoppingBasket} from "./shopping-basket/remove-from-shopping-basket";
+import {SET_ORDER_BILLING_ADDRESS, SetOrderBillingAddressAction, onSetOrderBillingAddress} from "./checkout/set-order-billing-address";
+import {SET_ORDER_SHIPPING_ADDRESS, SetOrderShippingAddressAction, onSetOrderShippingAddress} from "./checkout/set-order-shipping-address";
 
 const newState: State = {
     session: {
@@ -16,7 +16,7 @@ const newState: State = {
     // currentOrderItems: []
 };
 
-export function app(state: State = newState, action: Action): State {
+function app(state: State = newState, action: Action): State {
     switch (action.type) {
         case ADD_TO_SHOPPING_BASKET:
             return onAddToShoppingBasket(state, action as AddToShoppingBasketAction);
@@ -29,4 +29,16 @@ export function app(state: State = newState, action: Action): State {
         default:
             return state;
     }
+}
+
+function init() {
+    // load sellableItems from server
+    // load sellableItemPrices from server
+    // load session from local storage
+    // load shoppingBasketItems from local storage
+    // set initial state
+}
+
+function getItemPrice(sellableItemId: string): number {
+    return null;
 }
